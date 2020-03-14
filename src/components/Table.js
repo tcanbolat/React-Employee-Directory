566b28dd1a323ff52cc6e56d4e1bb115baf57bdb
atcanbolat <kangalcanbolat@gmail.com>
2020-03-13 19:52:39 -0700

    updated sort to include phone number and birthdates

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,34 +3,51 @@ import UserRows from "./UserRows";
 // import TableHeader from "./TableHead";
 import API from "../utils/API.js";
 import "../styles/style.css";
-
 class Table extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      users: [],
+      sortDir: "asc"
     };
     this.sortBy = this.sortBy.bind(this);
   }
-
   componentDidMount() {
     API.randomUsers().then(answer => {
       console.log(answer.data.results);
       this.setState({ users: answer.data.results });
     });
   }
-
   sortBy(key, data) {
     console.log(key);
-
     this.setState({
-      users: data.sort((a, b) =>
-       a.name.first.localeCompare(b.name.first))
-       
+      users: data.sort((a, b) => { 
+        if (key === 'phone') {
+          if (this.state.sortDir === "asc") {
+            return a.phone.localeCompare(b.phone);
+          } else {
+            return b.phone.localeCompare(a.phone);
+          }
+        }
+        if (key === 'firstname') {
+          if (this.state.sortDir === "asc") {
+            return a.name.first.localeCompare(b.name.first);
+          } else {
+            return b.name.first.localeCompare(a.name.first);
+          }
+        }
+        if (key === 'dob') {
+          if (this.state.sortDir === "asc") {
+            return a.dob.date.localeCompare(b.dob.date);
+          } else {
+            return b.dob.date.localeCompare(a.dob.date);
+          }
+        }
+      }
+       )
     });
-
+    this.setState({ sortDir: this.state.sortDir === "asc" ? "desc" : "asc" });
   }
-
   render() {
     return (
       <div className="table-responsive">
@@ -38,10 +55,10 @@ class Table extends React.Component {
           <thead>
             <tr>
               <th>image</th>
-              <th onClick={() => this.sortBy("user.name.first", this.state.users)}>Name</th>
-              <th onClick={() => this.sortBy("user.phone", this.state.users)}>Phone</th>
+              <th onClick={() => this.sortBy("firstname", this.state.users)}>Name</th>
+              <th onClick={() => this.sortBy("phone", this.state.users)}>Phone</th>
               <th>Email</th>
-              <th>Date of Birth</th>
+              <th onClick={() => this.sortBy("dob", this.state.users)}>Date of Birth</th>
             </tr>
           </thead>
           <tbody>
@@ -52,5 +69,4 @@ class Table extends React.Component {
     );
   }
 }
-
 export default Table;
